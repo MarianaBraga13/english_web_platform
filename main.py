14ac484f8cf93806810222580522aeb3640d986a
Mariana Braga <maribragad@gmail.com>
2025-10-17 18:41:54 -0300

    Site funcional em servidor local

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from website.__init__ import create_app
+from website import create_app
 
 # O ambiente local está como padrão,
-# Caso queira utilizar o ambiente remoto, basta subistituir por:
+# Caso queira utilizar o ambiente remoto, basta subistituir o ARGS* por:
 # app = create_app("remoto") ou app = create_app(ambiente="remoto")
 
 app = create_app()
--- website/test_db_config.py
+++ website/test_db_config.py
@@ -1,13 +1,9 @@
+from config import load_encrypted_config  # sem 'website.'
 from pathlib import Path
-from website.config import load_encrypted_config
 
-# Caminho para o arquivo database.ini
-config_file = Path(__file__).parent / 'website' / 'database.ini'
-
-# Carrega e descriptografa as configurações
+config_file = Path(__file__).parent / 'database.ini'
 db_config = load_encrypted_config(config_file)
 
-# Exibe as configurações descriptografadas
 print("Configurações carregadas do database.ini:")
 for key, value in db_config['postgresql'].items():
     print(f"{key}: {value}")
